54b4f113ff4f084c9796d38b3f28d132cc3bc0fa
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-09-12 10:55:55 -0700

    add language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,22 +1,37 @@
-var userFormEl =  document.querySelector("#user-form");
+var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 // submit form
 var formSubmitHandler = function(event) {
     // prevent page from refreshing
     event.preventDefault();
+
     // get value from input element
     var username = nameInputEl.value.trim();
 
     if (username) {
         getUserRepos(username);
+
         // clear old content
         repoContainerEl.textContent = "";
         nameInputEl.value = ""; 
     } else {
-        alert("Please enter a GitHub username")
+        alert("Please enter a GitHub username");
+    }
+};
+
+var buttonClickHandler = function(event) {
+    // get the language attribute from the clicked element
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
     }
 };
 
@@ -25,7 +40,7 @@ var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    // make a request to the url
+    // make a get request to url
     fetch(apiUrl)
     .then(function(response) {
         // request was successful
@@ -34,12 +49,30 @@ var getUserRepos = function(user) {
                 displayRepos(data, user);   
         });
     } else {
-        alert("Error: GitHub User Not Found");
+        alert("Error: " + response.statusText);
     }
 })
 .catch(function(error) {
-    alert ("Unable to connect to GitHub");
+    alert("Unable to connect to GitHub");
 });
+};
+
+var getFeaturedRepos = function(language) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+         response.json().then(function(data) {
+            displayRepos(data.items, language);
+         });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
 
 // display data
 var displayRepos = function(repos, searchTerm) {
@@ -50,12 +83,13 @@ var displayRepos = function(repos, searchTerm) {
   }
 
   repoSearchTerm.textContent = searchTerm;
+
    // loop over repos
    for (var i = 0; i < repos.length; i++) {
        // format repo name
        var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-       // create a container for each repo
+       // create a link for each repo
        var repoEl = document.createElement("a");
        repoEl.classList = "list-item flex-row justify-space-between align-center";
        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
@@ -84,14 +118,9 @@ var displayRepos = function(repos, searchTerm) {
 
        // append container to the dom
        repoContainerEl.appendChild(repoEl);
-       
    }
-   
 };
 
-
-};
-
-
-
+// add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
